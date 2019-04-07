63c60b8760817a197bdd0e0954321f0551088f1f
diana muajhed <diana.muj98@gmail.com>
2019-04-07 03:20:58 +0300

    select posts query

--- test.php
+++ test.php
@@ -1,11 +1,13 @@
 <?php 
 session_start();
 require('conn.php');
-		   $username=$_SESSION['username'];
-           $posts_check_query = "SELECT * FROM posts WHERE userid=2 or userid  in ( SELECT USERID2 FROM FRIENDSHIP WHERE USERID=2) order by datetimecurrent DESC";
-         $result = mysqli_query($conn, $posts_check_query);
-         echo mysqli_error($conn);
-         $posts = mysqli_fetch_assoc($result);
-         $text=$posts['posttext'];
-         echo "$text";
+$db;
+$connection=$db->conn;
+$userid=$_SESSION['userId'];
+$posts_check_query = "SELECT * FROM posts WHERE userid=$userid or userid  in ( SELECT USERID2 FROM FRIENDSHIP WHERE USERID=$userid) order by datetimecurrent DESC";
+$result = mysqli_query($connection, $posts_check_query);
+echo mysqli_error($connection);
+foreach ($result as  $row) {
+echo $row['postText']. "<br>";
+}
         ?>
