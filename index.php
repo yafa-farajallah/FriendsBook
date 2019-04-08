982f006b6f7bacefb11e47284513952155f6afd2
yafa <yafa.farajallah.1998@gmail.com>
2019-04-08 14:14:12 +0300

    edit navbar

--- index.php
+++ index.php
@@ -69,25 +69,27 @@ function DoAction($action)
 }
 session_start();
 if(isset($_SESSION['userId'])){
-
+  $id=$_SESSION['userId'];
+  $query ="SELECT * FROM userac where userId=$id";
+  $result=$db->SelectData($query);
+  $user=mysqli_fetch_array($result);
+  $firstName=$user[3];
+  $lastName=$user[4];
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>FriendsWorld</title>
 	<link rel="icon" href="images/caticon.png">
 	<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
-<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-<!------ Include the above in your HEAD tag ---------->
-
-
-<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
-<link href="css/index.css" rel="stylesheet" media="all">
-</head>
+  <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+  <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+  <link href="css/index.css" rel="stylesheet" media="all">
+  </head>
 <body>
 
-	<!--<h1> Welcome <?php echo $_SESSION['username'] ?></h1>-->
+	
 	<nav class=" navbar navbar-default pink">
   <div  class="container-fluid ">
     
@@ -95,8 +97,10 @@ if(isset($_SESSION['userId'])){
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div  class="collapse navbar-collapse " id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li ><a style="color: white;" href="#">profile<span class="sr-only">(current)</span></a></li>
-        <li ><a a style="color: white;"href="index.php">home</a></li>
+      <li ><a style="color: white;"href="index.php"><?php echo $firstName." ".$lastName ?></a></li>
+        <li ><a style="color: white;" href="#">My Profile<span class="sr-only">(current)</span></a></li>
+        <li ><a style="color: white;"href="index.php">Home Page</a></li>
+        
         
       </ul>
     
